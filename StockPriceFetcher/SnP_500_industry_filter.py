e2a5f984c2ac7171598f761e6a47719c5aea0b9f
i-am-kalrav <73673683+i-am-kalrav@users.noreply.github.com>
2024-03-21 02:12:35 +0530

    Update SnP_500_industry_filter.py

--- StockPriceFetcher/SnP_500_industry_filter.py
+++ StockPriceFetcher/SnP_500_industry_filter.py
@@ -40,6 +40,7 @@ st.write('Table Dimension: ' + str(df_selected_sector.shape[0]) + ' rows and ' +
 st.dataframe(df_selected_sector)
 
 
+#For separate downloading functionality
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
